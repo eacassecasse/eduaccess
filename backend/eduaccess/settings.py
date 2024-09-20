143e673d5d9a82cf48be6c4138b2bf656584db08
Edmilson de Azevedo Cassecasse <55765693+eacassecasse@users.noreply.github.com>
2024-09-20 11:38:00 +0200

    Update settings.py

--- backend/eduaccess/settings.py
+++ backend/eduaccess/settings.py
@@ -105,6 +105,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'https://eduaccess.up.railway.app',
     'https://eduaccess-5n4v3k9z5-eacassecasses-projects.vercel.app',
+    'https://eduaccess-git-frontend-development-eacassecasses-projects.vercel.app'
 ]  # Change this to a specific list of hosts in production
 
 CORS_ALLOW_METHODS = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS']
