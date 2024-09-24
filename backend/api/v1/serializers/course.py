d10c69753eae0dcdab3b00e1e0e5180a07198a38
Edmilson Cassecasse <>
2024-09-24 16:15:29 +0200

    feat: Returning the educator details when querying a course

--- backend/api/v1/serializers/course.py
+++ backend/api/v1/serializers/course.py
@@ -8,9 +8,21 @@ from ..models import Course
 
 
 class CourseSerializer(BaseSerializer):
-    educator = serializers.PrimaryKeyRelatedField(queryset=User.objects.filter(role='educator'))
+    educator = serializers.SerializerMethodField(read_only=True)
+    educator_id = serializers.PrimaryKeyRelatedField(queryset=User.objects.filter(role='educator'))
     students = UserSerializer(many=True, read_only=True)
 
     class Meta:
         model = Course
-        fields = ['id', 'title', 'prev_img', 'description', 'educator', 'students']
+        fields = ['id', 'title', 'prev_img', 'description', 'educator', 'educator_id', 'students']
+
+    def get_educator(self, obj):
+        educator = obj.educator
+        if educator and educator.role == 'educator':
+            return UserSerializer(educator).data
+        return None
+
+    def create(self, validated_data):
+        educator = validated_data.pop('educator_id')
+        course = Course.objects.create(educator=educator, **validated_data)
+        return course
